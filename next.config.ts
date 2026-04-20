c7556aa8d3b2a0e1cb904d088bd60aa58a327b4b
modelaiprob-tech <info@xanael.es>
2026-04-20 22:35:23 +0200

    config: add Medusa localhost image domain

--- next.config.ts
+++ next.config.ts
@@ -4,14 +4,13 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
   images: {
+    qualities: [75, 85, 90],
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'images.pexels.com' },
+      { protocol: 'http', hostname: 'localhost', port: '9000' },
     ],
   },
 };
