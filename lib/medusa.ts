b3bdaf62fbf6a03d7d71a29996b5e870628028fb
modelaiprob-tech <info@xanael.es>
2026-04-21 11:27:40 +0200

    fix: lazy-init Medusa client to avoid build failure without env vars

--- lib/medusa.ts
+++ lib/medusa.ts
@@ -1,6 +1,19 @@
 import Medusa from "@medusajs/js-sdk"
 
-export const medusa = new Medusa({
-  baseUrl: process.env.NEXT_PUBLIC_MEDUSA_URL!,
-  publishableKey: process.env.NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY!,
+let _medusa: Medusa | null = null
+
+export function getMedusa(): Medusa {
+  if (!_medusa) {
+    _medusa = new Medusa({
+      baseUrl: process.env.NEXT_PUBLIC_MEDUSA_URL || "http://localhost:9000",
+      publishableKey: process.env.NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY || "",
+    })
+  }
+  return _medusa
+}
+
+export const medusa = new Proxy({} as Medusa, {
+  get(_, prop) {
+    return getMedusa()[prop as keyof Medusa]
+  },
 })
