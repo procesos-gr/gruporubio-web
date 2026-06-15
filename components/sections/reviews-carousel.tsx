55d492c57df9256f4e6a6f9d37e9b4d5a00b3c96
modelaiprob-tech <info@xanael.es>
2026-06-15 10:45:34 +0200

    fix(reviews): enlazar al cid exacto de la ficha de Google Maps

--- components/sections/reviews-carousel.tsx
+++ components/sections/reviews-carousel.tsx
@@ -205,7 +205,7 @@ export function ReviewsCarousel() {
         {/* CTA */}
         <div style={{ textAlign: 'center', marginTop: 32 }}>
           <a
-            href="https://www.google.com/maps/search/?api=1&query=Grupo+Rubio+Tudela"
+            href="https://www.google.com/maps?cid=1770850957228213492"
             target="_blank"
             rel="noopener noreferrer"
             style={{ fontSize: 13, color: '#6B7280', textDecoration: 'none', fontWeight: 500 }}
