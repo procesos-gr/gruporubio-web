d1b707eae22751791a106a42e34044201977573e
modelaiprob-tech <info@xanael.es>
2026-06-17 17:36:08 +0200

    fix(ui): mover el toast de añadido al carrito a la esquina inferior izquierda

--- components/ui/Toast.tsx
+++ components/ui/Toast.tsx
@@ -6,6 +6,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
   return (
     <Sonner
       className="toaster group"
+      position="bottom-left"
       toastOptions={{
         classNames: {
           toast:
